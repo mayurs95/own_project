e2aa921baa486ed496f8826b28ed9892e84a600c
nikitamathews <119409574+nikitamathews@users.noreply.github.com>
2023-01-06 22:56:35 +1030

    Add files via upload

---
+++ git hub/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("your name is " + name);
+            Console.ReadKey();
+        }
+    }
+}
